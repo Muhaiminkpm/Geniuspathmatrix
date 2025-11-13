533824272645e5775fe8dc8edbe000222853c575
pathgenixedu <pathgenixedu@gmail.com>
2025-11-13 10:41:24 +0000

    fizx this

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,8 +1,5 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
-import {dotEnv} from 'genkit';
-
-dotEnv();
 
 export const ai = genkit({
   plugins: [googleAI({apiKey: process.env.GEMINI_API_KEY})],
