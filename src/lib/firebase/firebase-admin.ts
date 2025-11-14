b3e13ade31edbc62a9f74985b9c4d42c0c370195
pathgenixedu <pathgenixedu@gmail.com>
2025-11-14 06:43:51 +0000

    Try fixing this error: `Runtime Error: Error: The default Firebase app d

--- src/lib/firebase/firebase-admin.ts
+++ src/lib/firebase/firebase-admin.ts
@@ -1,12 +1,14 @@
 
-import { getApps, initializeApp, cert } from "firebase-admin/app";
-import { getFirestore } from "firebase-admin/firestore";
-import { getAuth } from "firebase-admin/auth";
+import { getApps, initializeApp, cert, App } from "firebase-admin/app";
+import { getFirestore, Firestore } from "firebase-admin/firestore";
+import { getAuth, Auth } from "firebase-admin/auth";
+
+let adminApp: App;
 
 // This prevents re-initializing the app on every hot-reload in development
 if (!getApps().length) {
   try {
-    initializeApp({
+    adminApp = initializeApp({
       credential: cert({
         projectId: process.env.FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
@@ -16,12 +18,15 @@ if (!getApps().length) {
     });
   } catch (error: any) {
     console.error('Firebase admin initialization error', error.stack);
+    // You might want to throw the error here to halt execution if Firebase admin is critical
+    throw error;
   }
+} else {
+  adminApp = getApps()[0];
 }
 
 
-const adminDb = getFirestore();
-const adminAuth = getAuth();
+const adminDb: Firestore = getFirestore(adminApp);
+const adminAuth: Auth = getAuth(adminApp);
 
 export { adminDb, adminAuth };
-
